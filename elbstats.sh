860a2edc2d27e7f2c4665c36d351158762f3c3f9
P-C Markovski <pcwriter@wearingmedia.com>
2020-07-21 07:20:13 +0000

    Added the disk statistics commands in 'elbstats.sh'.

--- elbstats.sh
+++ elbstats.sh
@@ -2,3 +2,14 @@
 
 # Purpose: Block storage monitor in AWS EC2 running Linux, using Bash script.
 # Author: P-C Markovski
+
+iotop -b -d 3
+
+iostat -p xvda1 1 4
+
+ioping -c 10 -s 1M /var/www
+
+du -c /var/log
+
+df -hT /dev/xvda1
+
