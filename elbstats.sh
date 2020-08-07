d3ca2fc20cbe92a7e6cb5f72244108f401be3b3a
P-C Markovski <pcwriter@wearingmedia.com>
2020-08-07 07:17:23 +0000

    Added software name, version, author and usage to echo statement (for $#==0).

--- elbstats.sh
+++ elbstats.sh
@@ -3,6 +3,13 @@
 # Purpose: Block storage monitor in AWS EC2 running Linux, using Bash script.
 # Author: P-C Markovski
 
+if [[ $# -eq 0 ]]; then
+	echo "ELBstats 0.5 - AWS Elastic Block Storage (ELB) stats"
+        echo "Author: P-C Markovski"
+	echo "Usage: elbstats.sh io[top/stat/ping] d[u/f]"
+	exit 0
+fi
+
 iotop -b -d 3
 
 iostat -p xvda1 1 4
